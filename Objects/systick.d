a49f875768aa82219ea9e37077c5021b0b509a4e
Oolayinka2002 <Olayinka.olowookere02@gmail.com>
2025-08-22 18:27:31 -0700

    Added a video.mp4 file to the project

--- Objects/systick.d
+++ Objects/systick.d
@@ -1,4 +1,4 @@
 ./objects/systick.o: Systick.c \
-  C:\Keil_v5\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\tm4c123gh6pm.h \
+  C:\Keil_v5\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
   C:\Keil_v5\ARM\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
   C:\Keil_v5\ARM\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
--- Systick.c
+++ Systick.c
@@ -1,13 +1,13 @@
-#include "tm4c123gh6pm.h"
+#include "TM4C123GH6PM.h"
 
 void delay_ms(uint32_t ms) {
     // Configure SysTick
     SysTick->LOAD = 16000 - 1;   // 16 MHz clock ? 1 ms per tick
     SysTick->VAL = 0;            // Clear current value
-    SysTick->CTRL = 5;           // Enable SysTick, no interrupts
+    SysTick->CTRL = 5;           // Enable SysTick, no interrupts(The counts starts from 16000 down to 0
 
     for(uint32_t i = 0; i < ms; i++) {
-        while((SysTick->CTRL & 0x10000) == 0);  // Wait for COUNT flag
+        while((SysTick->CTRL & 0x10000) == 0);  // Wait for the counter to reach 0
     }
 
     SysTick->CTRL = 0;  // Disable SysTick after delay
